c9c04300ffab95c421ff73b4baf1156101cdf4a3
sm3961 <65735941+sm3961@users.noreply.github.com>
2020-06-14 17:25:46 -0400

    Seperate input and output info in receipt.

--- shopping_cart.py
+++ shopping_cart.py
@@ -43,6 +43,7 @@ def to_usd(my_price):
 #INFO CAPTURE / INPUT
 
 total_price = 0
+selected_ids = []
 
 while True:
     selected_id = input("Please input a product identifier: ") #string
@@ -50,12 +51,22 @@ while True:
     if selected_id == "DONE":
         break
     else:
-        matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
-        matching_product = matching_products[0]
-        total_price = total_price + matching_product["price"]
-        print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        #matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+        #matching_product = matching_products[0]
+        #total_price = total_price + matching_product["price"]
+        #print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
+        selected_ids.append(selected_id)
 
 
 #INFO DISPLAY / OUTPUT
+
+# print(selected_ids)
+
+
+for selected_id in selected_ids:
+    matching_products = [p for p in products if str(p["id"]) == str(selected_id)]
+    matching_product = matching_products[0]
+    total_price = total_price + matching_product["price"]
+    print("SELECTED PRODUCT: " + matching_product["name"] + " " + str(matching_product["price"]))
 
 print("TOTAL PRICE: " + str(total_price))    #format as USD
